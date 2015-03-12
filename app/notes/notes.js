66051170abe27758bfe344b69067df6fb2decb39
Jason Beatty <jasondavidbeatty@gmail.com>
2015-03-12 15:11:01 -0400

    Add backend service to fetch and post notes

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -2,19 +2,50 @@
 
 var notelyBasePath = 'https://elevennote-nov-2014.herokuapp.com/api/v1/';
 var apiKey = '$2a$10$mDt9tMv4Qc2A5GAO6uS8b.f3SfpxjMgkF/t9IW2eXdPFPsV5LnAZ.';
+var noteModule = angular.module('notely.notes', ['ngRoute']);
 
-angular.module('notely.notes', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
+noteModule.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/notes', {
     templateUrl: 'notes/notes.html',
     controller: 'NotesController'
   });
-}])
+}]);
 
-.controller('NotesController', ['$http', '$scope', function($http, $scope) {
-  $http.get(notelyBasePath + 'notes.json?api_key=' + apiKey)
-    .success(function(notes_data) {
-      $scope.notes = notes_data;
+noteModule.controller('NotesController', ['$http', '$scope', 'NotesBackend', function($http, $scope, NotesBackend) {
+  NotesBackend.fetchNotes();
+
+  $scope.notes = function() {
+    return NotesBackend.getNotes();
+  };
+
+  $scope.commit = function() {
+    NotesBackend.postNote({
+      title: $scope.noteTitle,
+      body_html: $scope.noteBody
     });
+  };
+}]);
+
+noteModule.service('NotesBackend', ['$http', function($http) {
+  var notes = [];
+
+  this.getNotes = function() {
+    return notes;
+  };
+
+  this.fetchNotes = function() {
+    $http.get(notelyBasePath + 'notes.json?api_key=' + apiKey)
+    .success(function(notes_data) {
+        notes = notes_data;
+      });
+  };
+
+  this.postNote = function(note_data) {
+    $http.post(notelyBasePath + 'notes', {
+      api_key: apiKey,
+      note: note_data
+    }).success(function(new_note_data) {
+      notes.push(new_note_data);
+    });
+  };
 }]);
